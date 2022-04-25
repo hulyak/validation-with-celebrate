ee6b2519ecc0f1f31080f5bdb767bf9fca26f428
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2022-04-25 15:22:57 -0700

    init

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 const { celebrate, Joi, errors, Segments } = require("celebrate");
 
 const app = express();
+
 app.use(bodyParser.json());
 
+app.use(cookieParser("SECRET"));
+
 const { PORT = 3000 } = process.env;
-
-// app.post(
-//   "/signup",
-//   celebrate({
-//     [Segments.BODY]: Joi.object().keys({
-//       name: Joi.string().required(),
-//       age: Joi.number().integer(),
-//       role: Joi.string().default("admin"),
-//     }),
-//     [Segments.QUERY]: {
-//       token: Joi.string().token().required(),
-//     },
-//   }),
-//   (req, res) => {
-//     console.log(req.body);
-//     console.log(req.query.token);
-
-//     // res.send(req.body);
-//     // res.setHeader("Content-Type", "text/plain");
-//     res.status(200).send(req.body);
-//   }
-// );
 
 app.post(
   "/signup",
@@ -53,17 +36,53 @@ app.post(
   }
 );
 
+app.delete(
+  "/notes/:noteId",
+  celebrate({
+    // validate parameters
+    [Segments.PARAMS]: Joi.object().keys({
+      noteId: Joi.string().alphanum().length(12),
+    }),
+    [Segments.HEADERS]: Joi.object()
+      .keys({
+        // validate headers
+      })
+      .unknown(true),
+  }),
+  (req, res) => {
+    // ...
+    res.status(204).send();
+  }
+);
+
+app.get(
+  "/notes",
+  celebrate({
+    // validate parameters
+    [Segments.COOKIES]: Joi.object().keys({
+      name: Joi.string().alphanum().min(2).max(30),
+    }),
+    [Segments.SIGNEDCOOKIES]: Joi.object().keys({
+      jwt: Joi.string().alphanum().length(20),
+    }),
+  }),
+  function (req, res) {
+    res.cookie("name", "john", { httpOnly: true, maxAge: 3600000 });
+    // signed cookie
+    res.cookie("jwt", "snfsdfliuhewerewr4i3", { signed: true });
+
+    console.log("Cookies: ", req.cookies);
+    console.log("Signed Cookies: ", req.signedCookies);
+    res.send({ cookie: req.cookies.name, signedCookie: req.signedCookies.jwt });
+  }
+);
+
 app.use(errors());
 
-// Implement a error handler that will be called for us if the celebrate validation fails
-
-// app.use((error, req, res, next) => {
-//   if (error.joi) {
-//     return res.status(400).json({ error: error.joi.message });
-//   }
-
-//   return res.status(500).send(error);
-// });
+// global error handling
+app.use("*", (req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
